0b6c1e52fb78032cbdc29c3d6d594717dd7591c1
Naveen Kumar <34162787+naveen2314@users.noreply.github.com>
2024-08-22 18:14:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 // main.tf
 module "my_vm_instance" {
-  source  = "git::https://github.com/naveen2314/terraform-gcp-vm-module.git?ref=v1.0.0"
+  source  = "git::https://github.com/naveen2314/terraform-gcp-vm-module.git?ref=v1.2.0"
 
   name              = var.name
   project_id        = var.project_id
